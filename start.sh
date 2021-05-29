75745496e61b65682cef449070d1a842d305bd25
Dave Baker <dbaker@redhat.com>
2021-05-29 09:40:48 +0100

    Change default from tcp/8080 to tcp/7500

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ set -e
 #grep -r -q '"sign"' /var/db/tang || /usr/libexec/tangd-keygen /var/db/tang
 #/usr/libexec/tangd-update /var/db/tang /var/cache/tang
 
-socat tcp-l:8080,reuseaddr,fork exec:"/usr/libexec/tangd /var/db/tang"
+socat tcp-l:7500,reuseaddr,fork exec:"/usr/libexec/tangd /var/db/tang"
 
